e11523033dd22f7833f0037f69ec51487bf38bf6
Matt Green <verde.mateo.a@gmail.com>
2018-07-10 14:17:15 -0400

    [Setup] Cleaned up server.js

--- server.js
+++ server.js
@@ -1,23 +1,19 @@
 const os = require('os');
 const shell = require('shelljs');
 
+/*
+ * This script serves as setup and run for the Express Server.
+ */
 
-/*ng build --base-href /matt-green/ --aot   
-ng build --base-href /rpdr/ --aot
-node app.js*/
-
-var isWin = (os.platform() === 'win32');
-if (isWin) {
-    shell.cd('matt-green')
-    console.log('Building matt-green now.........................')
-    shell.exec('ng build --base-href /matt-green/ --aot', function (code, stdout, stderr) {
-        shell.cd('../rpdr-fl')
-        console.log('Building rpdr-fl now............................')
-        shell.exec('ng build --base-href /rpdr-fl/ --aot', function (code, stdout, stderr) {
-            shell.cd('..')
-            shell.exec('node app.js')
-        });
+console.log('Building Angular Apps')
+shell.cd('matt-green')
+console.log('Building matt-green now.........................')
+shell.exec('ng build --base-href /matt-green/ --aot', function (code, stdout, stderr) {
+    shell.cd('../rpdr-fl')
+    console.log('Building rpdr-fl now............................')
+    shell.exec('ng build --base-href /rpdr-fl/ --aot', function (code, stdout, stderr) {
+        shell.cd('..')
+        shell.exec('node app.js')
     });
-}
-
+});
 
